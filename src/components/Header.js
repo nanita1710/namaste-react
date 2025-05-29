dabebdd2805fb4ca9aeeaf36269722b741b8e30f
Nanita <nanita.1@globallogic.com>
2025-05-29 19:03:51 +0530

    Epi 7 changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/contants";
+import { Link } from "react-router";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
   return (
     <div className="header">
       <div className="logo-container">
@@ -8,10 +11,24 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About us</li>
-          <li>Contact us</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact us</Link>
+          </li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() =>
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login")
+            }
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
