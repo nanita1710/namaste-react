6f9921af97d99d1cacb16e06d8091b6cad2c106b
Nanita <nanita.1@globallogic.com>
2025-06-09 18:17:37 +0530

    changes

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,21 +1,41 @@
-const ItemsList = ({items}) =>{
-    console.log(items);
-    return (
-        <div>
-            <ul className="list-none">
-                {items.map((item) => (
-                    <li key={item.card.info.id} className="p-2 border-b border-gray-200 text-left">
-                        <div className="flex justify-between items-center">
-                            <span className="font-semibold py-1">{item.card.info.name}</span>
-                            <span className="text-gray-500">₹{item.card.info.price / 100}</span>
-                        </div>
-                        <p className="text-xs text-gray-600">{item.card.info.description}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+import { CDN_URL } from "../utils/contants";
 
+const ItemsList = ({ items }) => {
+  console.log(items);
+  return (
+    <div>
+      <ul className="list-none">
+        {items.map((item) => (
+          <li
+            key={item.card.info.id}
+            className="p-2 border-b border-gray-200 text-left flex justify-between"
+          >
+            <div className="w-10/12 p-4">
+              <div className="flex justify-between items-center">
+                <span className="font-semibold py-1">
+                  {item.card.info.name}
+                </span>
+                <span className="text-gray-500">
+                  ₹{item.card.info.price / 100}
+                </span>
+              </div>
+              <p className="text-xs text-gray-600">
+                {item.card.info.description}
+              </p>
+            </div>
+            <div className="w-2/12 p-4">
+              <div className="absolute">
+                <button className="p-2 bg-black text-white rounded-lg shadow-lg m-auto">
+                  Add+
+                </button>
+              </div>
+              <img src={CDN_URL + item.card.info.imageId} className=" rounded-lg"/>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default ItemsList;
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,20 +1,27 @@
+import { useState } from "react";
 import ItemsList from "./ItemsList";
 
-const RestaurantCategory = ({data}) =>{
-    return (
-        <div>
-            <div className="w-6/12 mx-auto my-6 bg-gray-50 shadow-lg p-4 ">
-            <div className="flex justify-between">
-                <span className="font-bold">{data.title} ({data.itemCards.length})</span>
-                <span className="">⬇️</span>
-            </div>
-            <div>
-                <ItemsList items={data.itemCards}/>
-            </div>
-            </div>
-            
+const RestaurantCategory = ({ data }) => {
+  const [showItems, setShowItems] = useState(false);
+  const handleClick = () => {
+    setShowItems(!showItems);
+  };
+  return (
+    <div>
+      <div className="w-6/12 mx-auto my-6 bg-gray-50 shadow-lg p-4 ">
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={handleClick}
+        >
+          <span className="font-bold">
+            {data.title} ({data.itemCards.length})
+          </span>
+          <span className="">{showItems ? "⬆️" : "⬇️"}</span>
         </div>
-    )
-}
+        {showItems && <ItemsList items={data.itemCards} />}
+      </div>
+    </div>
+  );
+};
 
 export default RestaurantCategory;
